12b590f02c0bbdeaab6e158421eeb6a9a5ce60b0
chris antoinette <christoanto2000@gmail.com>
2017-11-11 11:03:06 -0800

    Adding correct location listener
    Correct location listener will assist you to track the changing locations.

--- ParkingAndroidapp/app/src/main/java/com/chris/parkingandroidapp/fragmentMap.java
+++ ParkingAndroidapp/app/src/main/java/com/chris/parkingandroidapp/fragmentMap.java
@@ -1,10 +1,12 @@
 package com.chris.parkingandroidapp;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -12,7 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.location.LocationListener;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -22,6 +24,8 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.LocationListener;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -63,13 +67,6 @@ public class fragmentMap extends Fragment  implements LocationListener{
                 googleMap.setMyLocationEnabled(true);
                 showMap();
 
-//                // For dropping a marker at a point on the Map
-//                LatLng sydney = new LatLng(-34, 151);
-//                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-//
-//                // For zooming automatically to the location of the marker
-//                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-//                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
 
@@ -101,7 +98,6 @@ public class fragmentMap extends Fragment  implements LocationListener{
     }
 
     public void showMap() {
-
         // Enable Zoom
         googleMap.getUiSettings().setZoomGesturesEnabled(true);
 
@@ -114,6 +110,7 @@ public class fragmentMap extends Fragment  implements LocationListener{
         LocationManager mLocationManager = (LocationManager)getActivity().getApplicationContext().getSystemService(LOCATION_SERVICE);
         List<String> providers = mLocationManager.getProviders(true);
         Location bestLocation = null;
+        String bestProvider = new String();
         for (String provider : providers) {
             Location l = mLocationManager.getLastKnownLocation(provider);
             if (l == null) {
@@ -122,18 +119,22 @@ public class fragmentMap extends Fragment  implements LocationListener{
             if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
                 // Found best last known location: %s", l);
                 bestLocation = l;
+                bestProvider = provider;
             }
         }
         if (bestLocation != null) {
             onLocationChanged(bestLocation);
         }
+        // Register for location changes
+        mLocationManager.requestLocationUpdates(bestProvider, 5000, 0,  this);
+
+        // Get this location's parking spots
         DBhelper dbhelper = new DBhelper();
         List<LatLng> parkingLocations = dbhelper.findParkingSpaces(bestLocation);
         plotParkingLocations(parkingLocations);
     }
     private void plotParkingLocations(List<LatLng> parkingLocations) {
-        for(int i = 0; i < parkingLocations.size(); i++)
-        {
+        for(int i = 0; i < parkingLocations.size(); i++) {
             googleMap.addMarker(new MarkerOptions().position(parkingLocations.get(i)).title("Parking Spot: "+ i));
         }
     }
@@ -160,19 +161,24 @@ public class fragmentMap extends Fragment  implements LocationListener{
         //googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
     }
-//    @Override
-//    public void onProviderDisabled(String provider) {
-//
-//        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-//        startActivity(intent);
-//        Toast.makeText(getActivity().getBaseContext(), "Gps is turned off!!",
-//                Toast.LENGTH_SHORT).show();
-//    }
-//
-//    @Override
-//    public void onProviderEnabled(String provider) {
-//
-//        Toast.makeText(getActivity().getBaseContext(), "Gps is turned on!! ",
-//                Toast.LENGTH_SHORT).show();
-//    }
+    @Override
+    public void onProviderDisabled(String provider) {
+
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getActivity().getBaseContext(), "Gps is turned off!!",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+        Toast.makeText(getActivity().getBaseContext(), "Gps is turned on!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int number, Bundle bundle) {
+
+    }
 }
